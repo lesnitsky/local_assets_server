c7b4622d849aab901b01ec5b641df53f5e03ede3
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-06-15 23:01:38 +0300

    initial implementation

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
 # local_assets_server
 
-A new Flutter package project.
+HTTP Server which serves local assets
 
-## Getting Started
+![GitHub stars](https://img.shields.io/github/stars/lesnitsky/flutter_local_assets_server.svg?style=social)
+![Twitter Follow](https://img.shields.io/twitter/follow/lesnitsky_a.svg?label=Follow%20me&style=social)
 
-This project is a starting point for a Dart
-[package](https://flutter.dev/developing-packages/),
-a library module containing code that can be shared easily across
-multiple Flutter or Dart projects.
+## Installation
 
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.dev/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+```yml
+dependencies:
+  local_assets_server: ^1.0.0
+```
+
+```sh
+flutter packages get
+```
+
+## Usage
+
+TODO
+
+## LICENSE
+
+MIT
--- lib/local_assets_server.dart
+++ lib/local_assets_server.dart
@@ -1,7 +1,79 @@
-library local_assets_server;
+import 'dart:async';
+import 'dart:io';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/foundation.dart';
+import 'package:path/path.dart';
+import 'package:mime/mime.dart';
+
+import 'package:flutter/services.dart';
+
+class AssetsCache {
+  /// Assets cache
+  static Map<String, ByteData> assets = {};
+
+  /// Clears assets cache
+  static void clear() {
+    assets = {};
+  }
+}
+
+class LocalAssetsServer {
+  /// Server address
+  InternetAddress address;
+
+  /// Server port
+  int port;
+
+  /// Assets base path
+  String assetsBasePath;
+
+  LocalAssetsServer({
+    @required this.address,
+    @required this.port,
+    @required this.assetsBasePath,
+  }) {
+    assert(address != null);
+    assert(port != null);
+    assert(assetsBasePath != null);
+  }
+
+  /// Starts server
+  Future<InternetAddress> serve() async {
+    final server = await HttpServer.bind(this.address, this.port);
+    server.listen(_handleReq);
+
+    return server.address;
+  }
+
+  _handleReq(HttpRequest request) async {
+    String path = request.requestedUri.path.replaceFirst('/', '');
+
+    if (path == '') {
+      path = 'index.html';
+    }
+
+    try {
+      final data = await _loadAsset(path);
+      final name = basename(path);
+      final mime = lookupMimeType(name);
+
+      request.response.headers.add('Content-Type', '$mime; charset=utf-8');
+      request.response.add(data.buffer.asUint8List());
+
+      request.response.close();
+    } catch (err) {
+      request.response.statusCode = 404;
+      request.response.close();
+    }
+  }
+
+  Future<ByteData> _loadAsset(String path) async {
+    if (AssetsCache.assets.containsKey(path)) {
+      return AssetsCache.assets[path];
+    } else {
+      ByteData data = await rootBundle.load(join(assetsBasePath, path));
+      AssetsCache.assets[path] = data;
+      return data;
+    }
+  }
 }
